504aecc75423e1a9dd2813e52e060174a6b34d50
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-05-17 20:10:39 +0700

    feat: added unit test

--- src/database/tag.py
+++ src/database/tag.py
@@ -12,7 +12,6 @@ class Tag:
         return str((self.id, self.name))
 
     def save(self):
-        print(f"save |{self.name}|")
         cursor = DB.execute("INSERT INTO tag (name) VALUES (?)", (self.name.strip(),))
         self.id = cursor.lastrowid
 
@@ -26,7 +25,6 @@ class Tag:
     def get_all() -> List["Tag"]:
         cursor: Cursor = DB.execute("SELECT * FROM tag")
         res = cursor.fetchall()
-        print(f"Hasil res {res}")
         tag_list: List[Tag] = []
         for row in res:
             print(row)
